33e88ef07666d5e3ff4d302ae0819c53f960c2a9
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-20 18:16:20 -0700

    completes pirate ATP

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,23 @@
+class Pirate {
+  constructor (name, job = "Scallywag") {
+    this.name = name;
+    this.job = job;
+    this.cursed = false;
+    this.curseCounter = 0;
+    this.booty = 0;
+  }
+
+  commitHeinousAct () {
+    this.curseCounter += 1;
+    if (this.curseCounter > 2) {
+      this.cursed = true;
+    }
+  }
+
+  robShip () {
+    this.booty += 100;
+    return 'YAARRR!'
+  }
+}
+
+module.exports = Pirate;
